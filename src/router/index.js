94c94ced8fb8b169a0510481e01c0afa71b8d696
Brooks Pike <bpike7@gmail.com>
2019-03-29 12:50:03 -0500

    adjusted vue-router, removed auth module

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/Home';
 import HelloWorld from '@/components/HelloWorld';
 import AccountVerification from '@/components/AccountVerification';
 import Dash from '@/components/Dash';
-import Contact from '@/components/Contact';
-import SignUp from '@/components/SignUp';
-import SignIn from '@/components/SignIn';
 import Payment from '@/components/Payment';
 import Beneficiary from '@/components/Beneficiary';
 import Transaction from '@/components/Transaction';
 import Developer from '@/components/Developer';
 import Verify from '@/components/Verify';
 import FundAccount from '@/components/FundAccount';
-import auth from './auth';
-import ResetPassword from '@/components/ResetPassword';
 import MultiFactorAuth from '@/components/mfa';
 import AccountSettings from '@/components/AccountSettings';
 
@@ -23,9 +17,8 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
-      path: '/rf',
+      path: '/',
       component: Dash,
-      beforeEnter: AuthDatShit,
       children: [
         {
           path: 'dashboard',
@@ -58,11 +51,6 @@ export default new Router({
           name: 'FundAccount'
         },
         {
-          path: 'developer',
-          component: Developer,
-          name: 'Developer'
-        },
-        {
           path: 'verify',
           component: Verify,
           name: 'Verify'
@@ -79,50 +67,6 @@ export default new Router({
         }
       ]
     },
-    {
-      path: '/sign_in',
-      component: SignIn,
-      name: "SignIn"
-    },
-    {
-      path: '/sign_up',
-      component: SignUp,
-      name: "Sign Up"
-    },
-    {
-      path: '/contact',
-      component: Contact,
-      name: "Contact"
-    },
-    {
-      path: '/',
-      component: SignIn,
-      name: "Home"
-    },
-    {
-      path: "/sign_out",
-      name: "Sign Out",
-      beforeEnter (to, from, next) {
-        auth.logout()
-        next('/')
-      }
-    },
-    {
-      path: '/reset_password/:token',
-      component: ResetPassword,
-      name: "Reset Password"
-    }
   ]
 })
 
-// this was amazing https://github.com/vuejs/vue-router/blob/dev/examples/auth-flow/app.js
-function AuthDatShit (to, from, next) {
-  if (auth.loggedIn()) {
-    next();
-  } else {
-    next({
-      path: '/sign_in',
-      query: { redirect: to.fullPath }
-    });
-  }
-}
